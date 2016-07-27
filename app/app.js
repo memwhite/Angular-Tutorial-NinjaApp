754e7edfa946326981399742d5003d44c22beb05
Meghan White <meghanwhite11@gmail.com>
2016-07-27 14:34:26 -0400

    Angular Tutorial Ninja App

---
+++ app/app.js
@@ -0,0 +1,60 @@
+var myNinjaApp = angular.module('myNinjaApp', ['ngRoute']);
+
+//config does things before everything loads
+myNinjaApp.config(['$routeProvider', function($routeProvider){
+
+  $routeProvider
+    .when('/home', {
+      templateUrl:'views/home.html',
+      controller: 'NinjaController'
+    })
+    .when('/directory', {
+      templateUrl: 'views/directory.html',
+      controller: 'NinjaController'
+    }).otherwise({
+      redirectTo: '/home'
+    });
+
+}]);
+
+myNinjaApp.directive('randomNinja', [function(){
+
+  return {
+    restrict: 'E',
+    scope: {
+      ninjas: '=',
+      title: '='
+    },
+    templateUrl: 'views/random.html',
+    controller: function($scope){
+      $scope.random = Math.floor(Math.random() * 4);
+    }
+  };
+}]);
+
+
+myNinjaApp.controller("NinjaController", ['$scope', '$http', function($scope, $http){
+
+  $scope.removeNinja = function(ninja){
+    var removedNinja = $scope.ninjas.indexOf(ninja);
+    $scope.ninjas.splice(removedNinja, 1);
+  };
+
+  $scope.addNinja = function(){
+    $scope.ninjas.push({
+    name: $scope.newninja.name,
+    belt: $scope.newninja.belt,
+    rate: parseInt($scope.newninja.rate),
+    available: true
+    });
+
+    $scope.newninja.name = "";
+    $scope.newninja.belt = "";
+    $scope.newninja.rate = "";
+  };
+
+  $http.get('data/ninjas.json').success(function(data){
+    $scope.ninjas = data;
+  });
+
+}]);
